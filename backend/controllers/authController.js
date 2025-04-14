0ffc59692eef1d39da63cfcfc3d77c414051ffcc
Kanak Ramapure <kanak1803@gmail.com>
2025-04-14 15:30:59 +0530

    add env

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+const jwtSecret = process.env.JWT_SECRET;
 
 const dummyUser = {
   username: "user1",
@@ -12,7 +13,9 @@ exports.login = (req, res) => {
   // Check if the username and password match the dummy data
   if (username === dummyUser.username && password === dummyUser.password) {
     // Create a JWT token
-    const token = jwt.sign({ username }, "your_secret_key", { expiresIn: "1h" });
+    const token = jwt.sign({ username }, jwtSecret, {
+      expiresIn: "1h",
+    });
     return res.json({ token });
   }
 
