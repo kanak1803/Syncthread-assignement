c85dcd6cb24e198a8a875656f8bf09aea108599a
Kanak Ramapure <kanak1803@gmail.com>
2025-04-15 13:40:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,4 +17,4 @@ app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/map", mapRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on pOrt ${PORT}`));
